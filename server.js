46910c694a0939e12f1b7ab5377b2570146c0b1d
pickledbroccoli <163914034+pickledbroccoli@users.noreply.github.com>
2024-05-04 20:00:20 +0200

    add route for random GET and implement helper function for quote-by-person

--- server.js
+++ server.js
@@ -2,9 +2,25 @@ const express = require('express');
 const app = express();
 
 const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const { getRandomElement, getQuotesByPerson } = require('./utils');
 
 const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/api/quotes/random', (req, res, next) => {
+    const randomQuoteElement = getRandomElement(quotes);
+    res.send({
+        quote: randomQuoteElement
+    });
+
+});
+
+app.get('/api/quotes', (req, res, next) => {});
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
--- utils.js
+++ utils.js
@@ -3,6 +3,22 @@ const getRandomElement = arr => {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+const getQuotesByPerson = (arr, dude) => {
+  if (!Array.isArray(arr)) throw new Error('Expected an array');
+  let dudeQuotes = [];
+  for (let i=0; i < arr.length; i++) {
+    if (arr[i].person === dude) {
+      dudeQuotes.push(arr[i]);
+    }
+  }
+  if(dudeQuotes.length){
+    return dudeQuotes;
+  } else {
+    return false;
+  }
+}
+
+
 module.exports = {
-  getRandomElement
+  getRandomElement, getQuotesByPerson
 };
